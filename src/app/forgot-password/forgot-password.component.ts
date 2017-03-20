efadb333f0a4b4fab8404cd6baf5d950cc53bc36
Manpreet Singh <manpreet.singh@smartdatainc.net>
2017-03-20 11:11:43 +0530

    forgot password service add

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ForgotPasswordService } from './forgot-password.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.css'],
+  providers: [ForgotPasswordService]
 })
 export class ForgotPasswordComponent implements OnInit {
-  private user = {};	
-  constructor(private router : Router) { }
+  private email:any;	
+  constructor(private router : Router,private forgotPasswordService: ForgotPasswordService) { }
 
   ngOnInit() {
   }
 
   forgotPassword() {
-  	this.router.navigate(['/login']);
+        this.forgotPasswordService.forgot(this.email)
+                           .subscribe(
+                               comments => this.router.navigate(['/login']),
+                                err => {
+                                    console.log("error",err);
+                                    this.router.navigate(['/login']);
+                                });
   }
 }
--- src/app/forgot-password/forgot-password.service.ts
+++ src/app/forgot-password/forgot-password.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 @Injectable()
 export class ForgotPasswordService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  
+  forgot(data) {
+  	return this.http.post('http://localhost:3000/forgotPassword',data)
+                      .map((res:Response) => res.json())
+  }
 
 }
